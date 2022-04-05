e6f6d33716b7f7e6c438a98ba20cb130693e5c1c
Niamul Hasan Zead <zead.ece2014@gmail.com>
2022-04-05 12:32:26 +0600

    project is initialized

---
+++ src/Components/HomeReviw/HomeReview.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import "./HomeReview.css";
+const HomeReview = () => {
+    return (
+        <>
+            <div>
+                <h1 className='Review-heading'>Read Our Valueable Reviews</h1>
+            </div>
+            <div className="btn">
+                <button>See All Reviews</button>
+            </div>
+        </>
+    );
+};
+
+export default HomeReview;
